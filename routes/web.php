299e989c53920646031b0af2eafb02ac60a62b2a
Yong4-Hattori <hika1317@gmail.com>
2023-02-28 22:07:22 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::middleware('auth')->group(function () {
 });
 
 // LINE メッセージ受信
-Route::post('/line/webhook{task}', [LineRegistrationController::class,'webhook'])->name('line.webhook');
+Route::post('/line/webhook', [LineRegistrationController::class,'webhook'])->name('line.webhook');
 // LINE メッセージ送信用
 Route::get('/line/message/{task}', [LineRegistrationController::class,'message']);
 
