cdb6424eae0370829418ea67db13583e4f22458f
Yong4-Hattori <hika1317@gmail.com>
2023-03-12 22:47:18 +0900

    READ ME追加

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,21 +1,21 @@
 <!-- body内だけを表示しています。 -->
-      <x-app-layout>
-<body>
-    <h1 class="title">編集画面</h1>
-    <div class="content">
-        <form action="/tasks/{{ $task->id }}" method="POST">
-            @csrf
-            @method('PUT')
-            <div class='task_title'>
-                <input type='text' name='task[title]' value="{{ $task->title }}">
-            <div class='points'>
-                <input type="number" name="task[point]" value="{{ $task->point }}">
-            </div>
-            <div class='task_body'>
-                <input type='text' name='task[body]' value="{{ $task->body }}">
-            </div>
-            <input type="submit" value="store">
-        </form>
-    </div>
-</body>
-      </x-app-layout>
+<x-app-layout>
+    <body>
+        <h1 class="title">編集画面</h1>
+        <div class="content">
+            <form action="/tasks/{{ $task->id }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class='task_title'>
+                    <input type='text' name='task[title]' value="{{ $task->title }}">
+                <div class='points'>
+                    <input type="number" name="task[point]" value="{{ $task->point }}">
+                </div>
+                <div class='task_body'>
+                    <input type='text' name='task[body]' value="{{ $task->body }}">
+                </div>
+                <input type="submit" value="store">
+            </form>
+        </div>
+    </body>
+</x-app-layout>
